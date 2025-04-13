c99a4f26f9e28b187212a0af97e79633057efd54
RecryDv <166589923+RecryDv@users.noreply.github.com>
2025-04-13 17:43:27 +0300

    Update mandarinka.lua

--- mandarinka.lua
+++ mandarinka.lua
@@ -1,27 +1,14 @@
 local TweenService = game:GetService("TweenService")
 local StarsUtil = require(game.ReplicatedStorage.Shared.Utils.Stats.StatsUtil)
 local LocalData = require(game.ReplicatedStorage.Client.Framework.Services.LocalData)
-local args = {
-    [1] = "Teleport",
-    [2] = "Workspace.Worlds.The Overworld.Islands.Twilight.Island.Portal.Spawn"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer(unpack(args))
-
-task.wait(1)
-local coins_chunker = nil
-for _, pos_chunker in pairs(workspace.Rendered:GetChildren()) do
-if pos_chunker.Name == "Chunker" and #pos_chunker:GetChildren() > 3 then
-coins_chunker = pos_chunker
-end
-end
 
 
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/RecryDv/sh0velprod/refs/heads/main/UIlibrary.luau"))()
 local ui = lib.CreateWindow(1337, {
-	Title = "Bubble gum simulator infinity v0.1",
+	Title = "Bubble gum simulator infinity v0.2",
 	Auth = "by sh0vel",
-	
+    Size = UDim2.fromOffset(540, 365)
+
 })
 
 local AutoFarm = ui.CreateTab("Auto Farm")
@@ -32,7 +19,7 @@ local Collectables = AutoFarm.AddSection("Collectables")
 local data = {
 	auto_blow = false,
 	auto_sell = false,
-    auto_collect = false,
+	auto_collect = false,
 }
 
 Bubble.AddToggle({
@@ -69,23 +56,12 @@ Bubble.AddToggle({
 	end,
 })
 
-Collectables.AddToggle({
-    Title = "Auto collect coins & gems",
-    Callback = function(val)
-    data.auto_collect = val
-    task.spawn(function()
-			while data.auto_collect do
-				pcall(function()  
-                
-                for _, l in pairs(workspace.Rendered.Chunker_1:GetChildren()) do
-                l:Destroy()
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Pickups"):WaitForChild("CollectPickup"):FireServer(l.Name)
-                end
-                end)
-
-                task.wait(2)
-
-			end
+local old_formula = require(game:GetService("ReplicatedStorage").Shared.Utils.Stats.StatsUtil).GetPickupRange
+Collectables.AddButton({
+	Title = "Auto collect",
+	Callback = function(val)
+		hookfunction(StarsUtil.GetPickupRange, function()
+			return 13371337
 		end)
-    end
+	end
 })
