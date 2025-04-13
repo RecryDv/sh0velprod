fc34ccd3376ee781ff7b14bcbf2e638580fb98b7
RecryDv <166589923+RecryDv@users.noreply.github.com>
2025-04-13 17:09:14 +0300

    Create mandarinka.lua

---
+++ mandarinka.lua
@@ -0,0 +1,53 @@
+local TweenService = game:GetService("TweenService")
+local StarsUtil = require(game.ReplicatedStorage.Shared.Utils.Stats.StatsUtil)
+local LocalData = require(game.ReplicatedStorage.Client.Framework.Services.LocalData)
+
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/RecryDv/sh0velprod/refs/heads/main/UIlibrary.luau"))()
+local ui = lib.CreateWindow(1337, {
+	Title = "Bubble gum simulator infinity v0.1",
+	Auth = "by sh0vel",
+	
+})
+
+local AutoFarm = ui.CreateTab("Auto Farm")
+
+local Bubble = AutoFarm.AddSection("Auto Bubble")
+
+local data = {
+	auto_blow = false,
+	auto_sell = false,
+}
+
+Bubble.AddToggle({
+	Title = "Auto blow bubbles",
+	Callback = function(val)
+		data.auto_blow = val
+		
+		task.spawn(function()
+			while data.auto_blow do
+				game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer("BlowBubble")
+				task.wait(0.5)
+			end
+		end)
+	end,
+})
+
+Bubble.AddToggle({
+	Title = "Auto sell bubbles",
+	Callback = function(val)
+		data.auto_sell = val
+
+		task.spawn(function()
+			while data.auto_sell do
+				task.wait(0.1)
+				pcall(function()
+					local data = LocalData:Get()
+					local old = game.Players.LocalPlayer.Character.PrimaryPart.CFrame
+					if data.Bubble.Amount >= StarsUtil:GetBubbleStorage(data) then
+						game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer("SellBubble")
+                     end
+				end)
+			end
+		end)
+	end,
+})
