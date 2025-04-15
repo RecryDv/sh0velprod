173a3e8fd851d0f84968851ace4109fe45cf3b52
RecryDv <166589923+RecryDv@users.noreply.github.com>
2025-04-15 06:28:18 +0300

    Update mandarinka.lua

--- mandarinka.lua
+++ mandarinka.lua
@@ -24,10 +24,9 @@ local function teleport(cfr)
 		local r1 = TweenService:Create(root, TweenInfo.new(10, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), {CFrame = CFrame.new(cfr.Position.X, root.Position.Y, cfr.Position.Z)})
 		r1:Play()
 		r1.Completed:Connect(function()
-			root.Anchored = true
-			TweenService:Create(root, TweenInfo.new(0.2), {CFrame = cfr}):Play()
-			task.wait(1)
-			root.Anchored = false
+			TweenService:Create(root, TweenInfo.new(0.2), {CFrame = cfr + Vector3.new(0, 15, 0)}):Play()
+			task.wait(0.2)
+			root.CFrame = root.CFrame + Vector3.new(0, 5, 0)
 		end)
 	end)
 
@@ -36,7 +35,7 @@ end
 
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/RecryDv/sh0velprod/refs/heads/main/UIlibrary.luau"))()
 local ui = lib.CreateWindow(1337, {
-	Title = "Bubble gum simulator infinity v0.337",
+	Title = "Bubble gum simulator infinity v0.5",
 	Author = "by sh0vel",
 	Size = UDim2.fromOffset(540, 365)
 
@@ -49,11 +48,23 @@ local Gifts = AutoFarm.AddSection("Gifts")
 local Collectables = AutoFarm.AddSection("Collectables")
 local Eggs = Rift.AddSection("Eggs")
 local Other = Rift.AddSection("Other")
+
+local Enchants = ui.CreateTab("Enchants")
+local EncConfig = Enchants.AddSection("Enchant Config")
+local EncGems = Enchants.AddSection("Method: Gems")
+local EncOther = Enchants.AddSection("Method: Rerolls")
+
 local data = {
 	auto_blow = false,
 	auto_sell = false,
 	auto_collect = false,
 	auto_gifts = false,
+	auto_enchant = {
+		enchant = "",
+		level = 0,
+		gems = false,
+		other = false,
+	}
 }
 
 Bubble.AddToggle({
@@ -177,5 +188,217 @@ Gifts.AddToggle({
 				
 			end
 		end)
+	end,
+})
+
+EncConfig.AddButton({
+	Title = "Open config",
+	Callback = function()
+		EncConfig.AddMultiTable({
+			Title = "Enchant Config",
+			Options = {
+				{
+					Title = "Enchant",
+					Callback = function(val)
+						data.auto_enchant.enchant = val
+					end,
+				},
+				
+				{
+					Title = "Enchant Level",
+					Callback = function(val)
+						data.auto_enchant.level = tonumber(val)
+					end,
+				},
+			}
+		})
+	end,
+})
+
+EncGems.AddToggle({
+	Title = "Auto enchant",
+	Callback = function(val)
+		data.auto_enchant.gems = val
+		if data.auto_enchant.gems then
+			local All_Enchants = require(game:GetService("ReplicatedStorage").Shared.Data.Enchants)
+			local l_name = data.auto_enchant.enchant:lower()
+			
+			local correct = false
+			
+			for id, _ in pairs(All_Enchants) do
+				if id == l_name then
+					correct = true
+					break
+				end
+			end
+
+			if not correct then
+				local msg = Instance.new("Message", workspace)
+				msg.Text = "Invalid enchant id! Avaible IDs: looter, bubbler, team-up, gleaming"
+
+				task.delay(1, function()
+					msg:Destroy()
+				end)
+				return
+			end
+			
+			local petID = ""
+
+			local old_nmc
+			old_nmc = hookmetamethod(game, "__namecall", function(self, ...)
+				local method = getnamecallmethod()
+				if method == "InvokeServer" then
+					local args = {...}
+
+					if args[1] == "RerollEnchants" and petID == "" then
+						petID = args[2]
+					end
+				end
+
+				return old_nmc(self, ...)
+			end)
+
+
+			local msg = Instance.new("Message", workspace)
+			msg.Text = "Enchant your pet (with using gems) and auto enchant will start!"
+
+			task.delay(5, function()
+				msg:Destroy()
+			end)
+
+
+			local found_enchant = false
+			repeat
+				task.wait()
+			until petID ~= ""
+
+			repeat
+				local save = LocalData:Get()
+				local petData
+				for _, pet in pairs(save.Pets) do
+					if pet.Id == petID then
+						petData = pet
+						break
+					end
+				end
+
+				if petData == nil then
+					break
+				end
+
+				local enchants = petData.Enchants or {}
+
+				for _, enc_data in pairs(enchants) do
+					print(enc_data.Id, enc_data.Level)
+					print(l_name, data.auto_enchant.level)
+					if enc_data.Id == l_name and enc_data.Level >= data.auto_enchant.level then
+						found_enchant = true
+					end
+				end
+
+				if not found_enchant then
+					game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Function"):InvokeServer("RerollEnchants", petID)
+				end
+
+				task.wait(0.015)
+			until found_enchant == true or data.auto_enchant.gems == false
+		end
+	end,
+})
+
+EncOther.AddToggle({
+	Title = "Auto enchant",
+	Callback = function(val)
+		data.auto_enchant.gems = val
+		if data.auto_enchant.gems then
+			local All_Enchants = require(game:GetService("ReplicatedStorage").Shared.Data.Enchants)
+			local l_name = data.auto_enchant.enchant:lower()
+
+			local correct = false
+
+			for id, _ in pairs(All_Enchants) do
+				if id == l_name then
+					correct = true
+					break
+				end
+			end
+
+			if not correct then
+				local msg = Instance.new("Message", workspace)
+				msg.Text = "Invalid enchant id! Avaible IDs: looter, bubbler, team-up, gleaming"
+
+				task.delay(1, function()
+					msg:Destroy()
+				end)
+				return
+			end
+
+			local petID = ""
+
+			local old_nmc
+			old_nmc = hookmetamethod(game, "__namecall", function(self, ...)
+				local method = getnamecallmethod()
+				if method == "InvokeServer" then
+					local args = {...}
+
+					if args[1] == "RerollEnchants" and petID == "" then
+						petID = args[2]
+					end
+				end
+
+				return old_nmc(self, ...)
+			end)
+
+
+			local msg = Instance.new("Message", workspace)
+			msg.Text = "Enchant your pet (with using gems) and auto enchant will start!"
+
+			task.delay(5, function()
+				msg:Destroy()
+			end)
+
+
+			local found_enchant = false
+			repeat
+				task.wait()
+			until petID ~= ""
+
+			repeat
+				local save = LocalData:Get()
+				local petData
+				for _, pet in pairs(save.Pets) do
+					if pet.Id == petID then
+						petData = pet
+						break
+					end
+				end
+
+				if petData == nil then
+					break
+				end
+
+				local enchants = petData.Enchants or {}
+
+				for _, enc_data in pairs(enchants) do
+					print(enc_data.Id, enc_data.Level)
+					print(l_name, data.auto_enchant.level)
+					if enc_data.Id == l_name and enc_data.Level >= data.auto_enchant.level then
+						found_enchant = true
+					end
+				end
+
+				if not found_enchant then
+					local args = {
+						[1] = "RerollEnchant",
+						[2] = petID,
+						[3] = 1
+					}
+
+					game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer(unpack(args))
+				end
+
+				task.wait(0.015)
+			until found_enchant == true or data.auto_enchant.gems == false
+		end
 	end,
 })
